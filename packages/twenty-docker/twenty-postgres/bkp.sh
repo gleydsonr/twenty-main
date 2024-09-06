9b5e75705ee90e69693ab9ed73a15e610ef40b3a
rodrigoantero <113361136+rodrigoantero@users.noreply.github.com>
2024-09-06 14:05:41 -0300

    Create bkp.sh

---
+++ packages/twenty-docker/twenty-postgres/bkp.sh
@@ -0,0 +1,2 @@
+/opt/bitnami/postgresql/bin/pg_dump -h 127.0.0.1 -U twenty -Fc -b -f /bitnami/postgresql/bkp/default.backup default
+/opt/bitnami/postgresql/bin/pg_dump -h 127.0.0.1 -U twenty -Fc -b -f /bitnami/postgresql/bkp/test.backup test
